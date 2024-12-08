aecd7e60350f3b4a6c40957dfa4b8fbbf6a03eb8
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2024-12-08 04:22:25 +0000

    web/に移動

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -26,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <AppRouterCacheProvider>
         {children}
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
---
+++ web/src/components/images/KuRecLogo.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image"
+import KuRecImage from "../../../assets/images/KuRec-logo.webp"
+
+export default function KuRecLogo() {
+    return (
+        <Image src={KuRecImage} alt="KuRec Logo" />
+    )
+}
