a4349792cead1cf0d9b4ba8b3d8d6c41bdd40ce3
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2024-12-08 04:24:48 +0000

    layout importミス修正

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
