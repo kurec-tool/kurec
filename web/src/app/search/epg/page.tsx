b75dfd93ef05451a699401bb4df3127bbc697275
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2024-12-09 16:17:47 +0000

    ページを用意

---
+++ web/src/app/search/epg/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import DefaultLayout from '@/component/Navigation/DefaultLayout';
+import { Box } from '@mui/joy';
+
+export default function Home() {
+  return (
+    <DefaultLayout>
+      <Box>あいうえお</Box>
+    </DefaultLayout>
+  );
+}
