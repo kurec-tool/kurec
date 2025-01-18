bc9c3e62f06e500f10748f0af5cfb7904205ade6
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2025-01-18 06:20:11 +0000

    不要になったメッセージ定義を消す

--- server/kurec-interface/src/message/mod.rs
+++ server/kurec-interface/src/message/mod.rs
@@ -1,20 +1,11 @@
 use mirakc_client::models::{MirakurunProgram, MirakurunService};
 use serde::{Deserialize, Serialize};
 
-use crate::ProgramDocument;
-
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct EpgProgramsUpdatedMessage {
     pub tuner_url: String,
     pub service: MirakurunService,
     pub programs: Vec<MirakurunProgram>,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct ProgramDocumentsUpdatedMessage {
-    pub tuner_url: String,
-    pub service: MirakurunService,
-    pub programs: Vec<ProgramDocument>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
