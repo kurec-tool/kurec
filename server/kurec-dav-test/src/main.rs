1ca154928504ba140132ff3bcd111b12eb29c8d2
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2025-04-17 01:12:41 +0000

    WebDAVサーバーのテスト

---
+++ server/kurec-dav-test/src/main.rs
@@ -0,0 +1,44 @@
+use std::io;
+
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use dav_server::{
+    actix::{DavRequest, DavResponse},
+    fakels::FakeLs,
+    localfs::LocalFs,
+    DavConfig, DavHandler,
+};
+
+pub async fn dav_handler(req: DavRequest, davhandler: web::Data<DavHandler>) -> DavResponse {
+    if let Some(prefix) = req.prefix() {
+        let config = DavConfig::new().strip_prefix(prefix);
+        davhandler.handle_with(config, req.request).await.into()
+    } else {
+        davhandler.handle(req.request).await.into()
+    }
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    env_logger::init();
+    let addr = "0.0.0.0:4918";
+    let dir = "../../.devcontainer/recorded";
+
+    let dav_server = DavHandler::builder()
+        .filesystem(LocalFs::new(dir, false, false, false))
+        .locksystem(FakeLs::new())
+        .build_handler();
+
+    println!("actix-web example: listening on {} serving {}", addr, dir);
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::new(
+                "%a %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" \"%{Range}i\"",
+            ))
+            .app_data(web::Data::new(dav_server.clone()))
+            .service(web::resource("/{tail:.*}").to(dav_handler))
+    })
+    .bind(addr)?
+    .run()
+    .await
+}
