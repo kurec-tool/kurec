d4bd9def079f2af1225981a278ba7ebbf87bb670
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2025-04-19 21:19:57 +0000

    docker初期化書き換え

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -10,3 +10,6 @@ mc alias set "${MINIO_ALIAS}_admin" $MINIO_URL $MINIO_ROOT_USER $MINIO_ROOT_PASS
 mc admin user add "${MINIO_ALIAS}_admin" $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 mc admin policy attach "${MINIO_ALIAS}_admin" readwrite --user $MINIO_USER
 mc alias set $MINIO_ALIAS $MINIO_URL $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+
+# devcontainerのユーザリマッピングの範囲外なので手動で直す
+sudo chown -R $USER:$USER /usr/local/cargo
